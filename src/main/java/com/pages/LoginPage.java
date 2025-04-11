5414044884285892418c55acb9ea5ae4b0332d24
BK-IT-Github <kadambalaji321@rocketmail.com>
2025-04-11 17:14:00 +0530

    The .gitignore file is modified and and placed at correct location.

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -1,9 +1,10 @@
 package com.pages;
 
 public class LoginPage {
-	
+
 	public void loginPage() {
 		System.out.println("This is Login Page");
+		System.out.println("Check for 2nd commit....");
 	}
 
 }
